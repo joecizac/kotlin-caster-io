3214400666aade1ef5b11493dfacf8ce07da88fc
josy issac <josy.issac@neosofttech.com>
2021-01-02 11:52:49 +0530

    first commit

---
+++ app/src/main/java/com/jozze/kotlinplayground/test/number_separator.kt
@@ -0,0 +1,6 @@
+package com.jozze.kotlinplayground.test
+
+fun main() {
+    val number = 1_00_000_0_0_0
+    println(number)
+}
---
+++ app/src/main/java/com/jozze/kotlinplayground/test/readline.kt
@@ -0,0 +1,7 @@
+package com.jozze.kotlinplayground.test
+
+fun main() {
+    println("What's your name?")
+    val name = readLine()
+    println("Hello $name!")
+}
